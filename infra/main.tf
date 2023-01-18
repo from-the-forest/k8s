ff29bf6c65af7dd5ea88fd6c245ef95bfae5e4a5
Joseph Cuffney <josephcuffney@gmail.com>
2023-01-18 16:09:49 -0500

    s3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -5,25 +5,30 @@ terraform {
       version = "2.25.2"
     }
   }
-}
-
-variable "do_token" {}
-
-provider "digitalocean" {
-  # Configuration options
-  token = var.do_token
-}
-
-resource "digitalocean_kubernetes_cluster" "ftf-cluster" {
-  name    = "ftf-cluster"
-  region  = "nyc1"
-  version = "1.25.4-do.0"
-
-  node_pool {
-    name       = "autoscale-worker-pool"
-    size       = "s-1vcpu-2gb"
-    auto_scale = true
-    min_nodes  = 1
-    max_nodes  = 3
+  backend "s3" {
+    bucket = "ftf-tf-state"
+    key    = "k8s/terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
+# variable "do_token" {}
+
+# provider "digitalocean" {
+#   # Configuration options
+#   token  = var.do_token
+#   region = "nyc1"
+# }
+
+# resource "digitalocean_kubernetes_cluster" "ftf-cluster" {
+#   name    = "ftf-cluster"
+#   version = "1.25.4-do.0"
+
+#   node_pool {
+#     name       = "autoscale-worker-pool"
+#     size       = "s-1vcpu-2gb"
+#     auto_scale = true
+#     min_nodes  = 1
+#     max_nodes  = 3
+#   }
+# }
