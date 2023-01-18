b263b0a2f580354ce3cf05f429ca653ea9884647
Joseph Cuffney <josephcuffney@gmail.com>
2023-01-18 15:10:08 -0500

    update size

--- infra/main.tf
+++ infra/main.tf
@@ -21,7 +21,7 @@ resource "digitalocean_kubernetes_cluster" "ftf-cluster" {
 
   node_pool {
     name       = "autoscale-worker-pool"
-    size       = "s-2vcpu-2gb"
+    size       = "s-1vcpu-2gb"
     auto_scale = true
     min_nodes  = 1
     max_nodes  = 3
