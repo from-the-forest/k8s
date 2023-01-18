7e08f0ce6cc2be3a0f5d2f08e0e8d1f260e2ffe2
Joseph Cuffney <josephcuffney@gmail.com>
2023-01-18 16:11:51 -0500

    added k8s

--- infra/main.tf
+++ infra/main.tf
@@ -12,23 +12,23 @@ terraform {
   }
 }
 
-# variable "do_token" {}
+variable "do_token" {}
 
-# provider "digitalocean" {
-#   # Configuration options
-#   token  = var.do_token
-#   region = "nyc1"
-# }
+provider "digitalocean" {
+  # Configuration options
+  token  = var.do_token
+  region = "nyc1"
+}
 
-# resource "digitalocean_kubernetes_cluster" "ftf-cluster" {
-#   name    = "ftf-cluster"
-#   version = "1.25.4-do.0"
+resource "digitalocean_kubernetes_cluster" "ftf-cluster" {
+  name    = "ftf-cluster"
+  version = "1.25.4-do.0"
 
-#   node_pool {
-#     name       = "autoscale-worker-pool"
-#     size       = "s-1vcpu-2gb"
-#     auto_scale = true
-#     min_nodes  = 1
-#     max_nodes  = 3
-#   }
-# }
+  node_pool {
+    name       = "autoscale-worker-pool"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 3
+  }
+}
